8fcc814870b3cd9ba403ee38d4b5f1923f6209bb
herpie <herpritsingh12@gmail.com>
2023-03-05 08:21:38 +0100

    event json converter

---
+++ SocialMedia-Post/SocialMedia-Query/Sm.Query.Infrastructure/Converter/EventJsonConverter.cs
@@ -0,0 +1,48 @@
+﻿using CQRS.Core.Events;
+using Post.Commen.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace Sm.Query.Infrastructure.Converter
+{
+    public class EventJsonConverter : JsonConverter<BaseEvent>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsAssignableTo(typeof(BaseEvent));
+        }
+        public override BaseEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (!JsonDocument.TryParseValue(ref reader, out var doc))
+            {
+                throw new JsonException($"Failed to parse {nameof(JsonDocument)}!");
+            }
+
+            if (!doc.RootElement.TryGetProperty("Type", out var type))
+            {
+                throw new JsonException("Could not detect the type of discriminator property!");
+            }
+
+            var typeDiscriminator = type.GetString();
+            var json = doc.RootElement.GetRawText();
+
+            return typeDiscriminator switch
+            {
+                nameof(PostCreatedEvent) => JsonSerializer.Deserialize<PostCreatedEvent>(json, options),
+                nameof(MessageUpdatedEvent) => JsonSerializer.Deserialize<MessageUpdatedEvent>(json, options),
+                nameof(PostLikedEvent) => JsonSerializer.Deserialize<PostLikedEvent>(json, options),
+                nameof(CommentAddedEvent) => JsonSerializer.Deserialize<CommentAddedEvent>(json, options),
+                nameof(CommentRemoveEvent) => JsonSerializer.Deserialize<CommentRemoveEvent>(json, options),
+                nameof(CommentUpdatedEvent) => JsonSerializer.Deserialize<CommentUpdatedEvent>(json, options),
+                nameof(PostRemovedEvent) => JsonSerializer.Deserialize<PostRemovedEvent>(json, options),
+                _ => throw new JsonException($"{typeDiscriminator}is not supported yet!")
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, BaseEvent value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
